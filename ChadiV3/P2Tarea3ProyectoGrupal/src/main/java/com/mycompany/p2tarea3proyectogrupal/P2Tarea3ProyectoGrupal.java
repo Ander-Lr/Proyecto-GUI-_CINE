63839b2543e6ce62c01f374f9406939b646ab40f
ShadyReyes <150307763+ShadyReyes@users.noreply.github.com>
2024-01-30 17:51:52 -0500

    Add files via upload

---
+++ ChadiV3/P2Tarea3ProyectoGrupal/src/main/java/com/mycompany/p2tarea3proyectogrupal/P2Tarea3ProyectoGrupal.java
@@ -0,0 +1,16 @@
+package com.mycompany.p2tarea3proyectogrupal;
+
+//import Ventanas.NewJFrame;
+import Ventanas.VenSesion;
+
+
+public class P2Tarea3ProyectoGrupal {
+
+    public static void main(String[] args) {
+       
+       VenSesion ventana=new VenSesion();
+        ventana.setVisible(true);
+       
+      
+    }
+}
